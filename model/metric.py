c2578098c64d9bce0335846b049e109c177ac2b0
Kirill Gusev <kirill@kendze.net>
2023-02-10 12:12:08 +0000

    Change mse to rmse

--- model/metric.py
+++ model/metric.py
@@ -1,8 +1,7 @@
 import torch
-import numpy as np
 
 
 def rmse(output, target):
     with torch.no_grad():
         metric = torch.nn.MSELoss()
-        return np.sqrt(metric(output, target))
+        return torch.sqrt(metric(output, target))
