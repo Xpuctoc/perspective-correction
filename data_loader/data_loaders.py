3d30897d2cb26c7154cd31aa0f5b3591333cd2c5
Kirill Gusev <kirill@kendze.net>
2023-05-30 20:22:58 +0000

    Add augmentations

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,9 +1,16 @@
+import albumentations as A
 from base import BaseDataLoader
 from data_loader.dataset import HomographyDataset
 
 
+transform = A.Compose([
+    A.CLAHE(p=0.5),
+    A.ColorJitter(p=0.5),
+    A.PixelDropout()
+])
+
 class HomographyDataLoader(BaseDataLoader):
     def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1):
         self.data_dir = data_dir
-        self.dataset = HomographyDataset(self.data_dir)
+        self.dataset = HomographyDataset(self.data_dir, transforms=transform)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
--- data_loader/dataset.py
+++ data_loader/dataset.py
@@ -17,8 +17,8 @@ class HomographyDataset(torch.utils.data.Dataset):
         image_name_clear = image_name[:image_name.rfind('.')]  # without .jpg
         image_path = os.path.join(self.data_path, 'images', image_name)
         img = cv2.imread(image_path)  # BGR
+        src_img = np.copy(img)
         image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        src_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         if self.transforms is not None:
             image = self.transforms(image=image)['image']
@@ -43,12 +43,6 @@ class HomographyDataset(torch.utils.data.Dataset):
 
         homography = (homography - mean) / std
 
-        # mean_abs = np.array([9.10049482e-01, 1.33448711e-01, 6.12820893e+01,
-        #                      6.29014048e-02, 8.47585815e-01, 1.43520874e+01,
-        #                      2.12661938e-04, 5.06282876e-04, 9.95821547e-01])
-        #
-        # homography = homography / mean_abs
-
         return torch.from_numpy(image).float(), src_img, torch.from_numpy(homography).float()
 
     def __len__(self):
@@ -56,10 +50,6 @@ class HomographyDataset(torch.utils.data.Dataset):
 
     def preprocess(self, image):
         normalize = A.Normalize(mean=(0.471, 0.438, 0.405), std=(0.259, 0.251, 0.251))
-        # crop = A.CenterCrop(504, 504)  # for dinov2-vit/14
         image = normalize(image=image)['image']
-        # image = crop(image=image)['image']
         image = image.transpose(2, 0, 1)
-        # image = (image - 127.5) / 128
-
         return image
